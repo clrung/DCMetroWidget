92febe0f98a74da7af60a35bfa24da4e617b8a53
Christopher Rung <clrung@gmail.com>
2016-04-27 21:41:32 -0400

    Initial Commit

---
+++ DCMetro/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DCMetro
+//
+//  Created by Christopher Rung on 4/27/16.
+//  Copyright © 2016 Christopher Rung. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DCMetro/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DCMetro
+//
+//  Created by Christopher Rung on 4/27/16.
+//  Copyright © 2016 Christopher Rung. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
