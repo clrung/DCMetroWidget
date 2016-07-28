044d6b80e3de08b67502c201bc94a3c9fba6b033
Christopher Rung <clrung@gmail.com>
2016-07-27 21:16:11 -0400

    misc build changes, shortened names of some metro stations

---
+++ Pods/Target Support Files/Pods-DC Metro /Pods-DC Metro -dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DC_Metro_ : NSObject
+@end
+@implementation PodsDummy_Pods_DC_Metro_
+@end
---
+++ Pods/Target Support Files/Pods-DC Metro /Pods-DC Metro -umbrella.h
@@ -0,0 +1,6 @@
+#import <Cocoa/Cocoa.h>
+
+
+FOUNDATION_EXPORT double Pods_DC_Metro_VersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_DC_Metro_VersionString[];
+
