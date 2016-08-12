ea97cf8a70d48a8d11346adbc9200d7d44f52f41
Christopher Rung <clrung@gmail.com>
2016-08-12 10:52:50 -0400

    setup .gitignore

--- Pods/Target Support Files/Pods-DC Metro/Pods-DC Metro-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DC_Metro : NSObject
-@end
-@implementation PodsDummy_Pods_DC_Metro
-@end
--- Pods/Target Support Files/Pods-DC Metro/Pods-DC Metro-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <Cocoa/Cocoa.h>
-
-
-FOUNDATION_EXPORT double Pods_DC_MetroVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_DC_MetroVersionString[];
-
